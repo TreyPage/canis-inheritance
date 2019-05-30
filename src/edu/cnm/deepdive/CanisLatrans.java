70538ae3cf7abe3ba4c1508ddc2283e6ba2c320c
Terrell Page <tpage46@gmail.com>
2019-05-30 10:28:35 -0600

    mod .gitignore and added CanisLatrans

---
+++ src/edu/cnm/deepdive/CanisLatrans.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive;
+
+public class CanisLatrans extends Canis {
+
+  @Override
+  public void hunt() {
+    System.out.println("Hunt small prey individually, larger prey in packs.");
+  }
+
+}
